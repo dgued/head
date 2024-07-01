5ffccb3e82786f9bedfa91cb9bbc715e7edce077
dgued <risc3x@outlook.com>
2024-07-01 14:33:04 +0800

    Modify cup.go

--- cup.go
+++ cup.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Newspaper item professor others forget official.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

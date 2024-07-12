9b625e894dc5a25c6da26e947914cf9203168347
dgued <risc3x@outlook.com>
2024-07-12 11:56:07 +0800

    Modify star.c

--- star.c
+++ star.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Especially marriage green design win budget.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

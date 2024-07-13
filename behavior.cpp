b3ecc185a016a721859593241c9bff5e9aa64351
dgued <risc3x@outlook.com>
2024-07-13 14:44:02 +0800

    Modify behavior.cpp

--- behavior.cpp
+++ behavior.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Three of later.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

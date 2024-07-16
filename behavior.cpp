e25c52be40d1f1dd6fa6345387cfda696ac4f234
dgued <risc3x@outlook.com>
2024-07-16 10:18:56 +0800

    Modify behavior.cpp

--- behavior.cpp
+++ behavior.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Machine still else.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

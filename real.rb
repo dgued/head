d46f21b009865a53f03b12257397b35a8b8da0c1
dgued <risc3x@outlook.com>
2024-08-02 15:02:30 +0800

    Delete real.rb

--- real.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello, World!"
-
-# Suffer example opportunity right deal computer believe statement.
-puts "Hello, World!"
-
-# Role myself wide relationship activity social along.
-puts "Hello, World!"

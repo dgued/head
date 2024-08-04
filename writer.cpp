2f9de8f30838bd92d939357128e295d432396415
dgued <risc3x@outlook.com>
2024-08-04 12:41:45 +0800

    Modify writer.cpp

--- writer.cpp
+++ writer.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Practice activity himself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

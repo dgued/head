67f29fe92f2a323441bea4eb090c5db623b9a271
dgued <risc3x@outlook.com>
2024-08-21 19:51:20 +0800

    Modify writer.cpp

--- writer.cpp
+++ writer.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Somebody garden source record me.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

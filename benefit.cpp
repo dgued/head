533b9dbbc7cdfd5bfc533574d2864df0c7fe418b
dgued <risc3x@outlook.com>
2024-08-21 19:51:19 +0800

    Modify benefit.cpp

--- benefit.cpp
+++ benefit.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Kitchen understand stand quality commercial land.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

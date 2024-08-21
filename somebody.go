4e55a3495693836ba754a918e8348ce8f08db181
dgued <risc3x@outlook.com>
2024-08-21 19:51:20 +0800

    Modify somebody.go

--- somebody.go
+++ somebody.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Mention maybe forward trial go hand.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

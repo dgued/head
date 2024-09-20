eb28fa729f3d106047cc4108fc2f318cdbfc8cf5
dgued <risc3x@outlook.com>
2024-09-20 19:44:23 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thousand probably form play wish new future.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

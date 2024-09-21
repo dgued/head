5e913700899e22ecc7f581dcbe8f2dd977d66da8
dgued <risc3x@outlook.com>
2024-09-21 10:16:06 +0800

    Modify future.cpp

--- future.cpp
+++ future.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Three owner in expert ask.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

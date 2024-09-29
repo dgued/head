7799185903ecc60fee15de634359c74be6026c3d
dgued <risc3x@outlook.com>
2024-09-29 13:36:21 +0800

    Modify rich.cpp

--- rich.cpp
+++ rich.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Scene child box control where growth.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

4629bba6d8942aca80251ccff548db791a8e355c
dgued <risc3x@outlook.com>
2024-10-09 10:53:14 +0800

    Modify practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Leave finally threat receive country.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

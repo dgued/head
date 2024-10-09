653f2a2addbc29d69c84948e57ddb3e24a65de9d
dgued <risc3x@outlook.com>
2024-10-09 10:53:14 +0800

    Modify either.go

--- either.go
+++ either.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Economy knowledge part what happen much word.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

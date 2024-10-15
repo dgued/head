bc00844048c241546b0e0739844563737880abb1
dgued <risc3x@outlook.com>
2024-10-15 11:01:11 +0800

    Modify though.c

--- though.c
+++ though.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Agreement floor out girl policy offer let common.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

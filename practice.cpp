3f1790ebf68e44987d0cd63a7e8c3824b6fbcc1d
dgued <risc3x@outlook.com>
2024-10-15 11:01:12 +0800

    Modify practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Job today thought identify citizen why example.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

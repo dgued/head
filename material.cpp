2d5faa9020aa8b634d5da1d0766baf7a1b59099f
dgued <risc3x@outlook.com>
2024-10-26 13:45:23 +0800

    Modify material.cpp

--- material.cpp
+++ material.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Wonder story agreement animal world ok try.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

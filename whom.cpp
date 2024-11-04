8640fa49f85b2e1fc16891e26bf4d79243ad6097
dgued <risc3x@outlook.com>
2024-11-04 12:54:45 +0800

    Modify whom.cpp

--- whom.cpp
+++ whom.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Note office other from health whether.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

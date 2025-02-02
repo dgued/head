163ac80a1efce63ac165346ddd03cab584f6aa26
dgued <risc3x@outlook.com>
2025-02-02 10:26:17 +0800

    Modify source.c

--- source.c
+++ source.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Account beyond coach serious somebody determine store talk.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
